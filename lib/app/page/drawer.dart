009fa055dc6413c63fe2ca6902266283a7dfaa59
odaki-macbook-air <odaki308@gmail.com>
2023-11-23 05:11:48 +0900

    ドロワーのリファクタ

--- lib/app/page/drawer.dart
+++ lib/app/page/drawer.dart
@@ -10,55 +10,55 @@ class AppDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     final appTheme = getAppTheme(context);
     return Drawer(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            width: double.infinity,
-            color: appTheme.appColors.drawerBackground,
-            child: DrawerHeader(
-              margin: const EdgeInsets.only(bottom: 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '家計簿アプリCopan',
-                    style: TextStyle(color: appTheme.appColors.secondaryText),
-                  ),
-                  Spacer(),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.account_circle,
-                        size: 32,
-                        color: Colors.white,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8),
-                        child: Text(
-                          User().name,
-                          style: TextStyle(
-                              color: appTheme.appColors.secondaryText),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(padding: EdgeInsets.only(bottom: 10))
-                ],
+        child: ListView(padding: EdgeInsets.zero, children: [
+      DrawerHeader(
+        decoration: BoxDecoration(
+          color: appTheme.appColors.drawerBackground,
+        ),
+        child: _HeaderContent(),
+      ),
+      ListTile(
+        title: const Text('家計簿の共有'),
+        onTap: () {},
+      ),
+      ListTile(
+        title: const Text('支払割合'),
+        onTap: () {},
+      ),
+    ]));
+  }
+}
+
+class _HeaderContent extends StatelessWidget {
+  const _HeaderContent();
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = getAppTheme(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '家計簿アプリCopan',
+          style: TextStyle(color: appTheme.appColors.secondaryText),
+        ),
+        Spacer(),
+        Row(
+          children: [
+            Icon(
+              Icons.account_circle,
+              size: 32,
+              color: Colors.white,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Text(
+                User().name,
+                style: TextStyle(color: appTheme.appColors.secondaryText),
               ),
             ),
-            //   ),
-          ),
-          ListTile(
-            title: const Text('家計簿の共有'),
-            onTap: () {},
-          ),
-          ListTile(
-            title: const Text('支払割合'),
-            onTap: () {},
-          ),
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
